8fb7b30bc1e4a2db08f10962fb6eb2e539695705
Isabel Paredes <iparedes314@gmail.com>
2023-02-07 15:38:17 +0100

    Update build instructions

--- env_vars.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-export ROS_VERSION=2
-export ROS_PYTHON_VERSION=3
-export ROS_DISTRO=humble
-export ROS_LOCALHOST_ONLY=1
-export RMW_IMPLEMENTATION=rmw_wasm_cpp
-export ROS_LOG_DIR="/home/ihuicatl/Robots/repos/MasterThesis/Middleware/workspace_underlay/ros_log"
